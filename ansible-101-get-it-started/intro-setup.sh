85a2a17a360ac3503edee238f419cfb79147340b
bitbauer <bauer_tweet@yahoo.com>
2018-09-24 09:19:46 +0200

    Updated

--- ansible-101-get-it-started/intro-setup.sh
+++ ansible-101-get-it-started/intro-setup.sh
@@ -1,4 +1,7 @@
-apt-get update
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
+echo "Running apt repository update ..."
+apt-get update >/dev/null
+
+echo "Installing ansible apt repository ..."
+apt-get install -y software-properties-common >/dev/null
+apt-add-repository -y ppa:ansible/ansible >/dev/null
+apt-get update >/dev/null
